77a78bf5fd8d6be7783b253cc256a964210ce41c
Your Name <you@example.com>
2018-06-23 15:44:33 -0700

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+$ (function(){
+  var onChangeInput = function(e){
+    var inputText = e.target.value;
+    console.log (inputText);
+    if (inputText === "") {
+      $(".photo").show();
+    } else {
+      $(".photo").hide();
+      var search = "."+ inputText;
+      $(search).show();
+    }
+
+  };
+  $("input").change(onChangeInput);
+});
